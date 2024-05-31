2e068710706d2b40a5ffaa7bce1d30267e784b43
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-05-31 08:44:17 -0400

    Agregando iconos de resdes sociales

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,111 @@
 <template>
   <v-app style="background-color: #EFEBE9;">
     <v-app-bar app color="grey-darken-4" dark>
-  <!-- Botón de menú para móviles -->
-  <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none"></v-app-bar-nav-icon>
-  <v-toolbar-title>
-    <v-img src="@/assets/logo.png" contain alt="Logo de la barbería"></v-img>
-  </v-toolbar-title>
+      <!-- Botón de menú para móviles -->
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <v-img src="@/assets/logo.png" contain alt="Logo de la barbería"></v-img>
+      </v-toolbar-title>
 
-  <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-  <!-- Menú de navegación para pantallas grandes -->
-  <v-btn text to="/" prepend-icon="mdi-home" class="text-capitalize d-none d-md-flex">Inicio</v-btn>
-  <v-btn text href="https://reservasbh.simplifies.cl" prepend-icon="mdi-calendar" class="text-capitalize d-none d-md-flex">Reserva</v-btn>
-  <v-btn text to="/servicios" prepend-icon="mdi-format-list-bulleted-triangle" class="text-capitalize d-none d-md-flex">Servicios</v-btn>
-  <v-btn text to="/sobre-nosotros" prepend-icon="mdi-account-group" class="text-capitalize d-none d-md-flex">Sobre nosotros</v-btn>
-  <v-btn text to="/sucursales" prepend-icon="mdi-store" class="text-capitalize d-none d-md-flex">Sucursales</v-btn>
-  <v-btn text href="#tienda-online" prepend-icon="mdi-store" class="text-capitalize d-none d-md-flex">Tienda online</v-btn>
+      <!-- Menú de navegación para pantallas grandes -->
+      <v-btn text to="/" prepend-icon="mdi-home" class="text-capitalize d-none d-md-flex">Inicio</v-btn>
+      <v-btn text href="https://reservasbh.simplifies.cl" prepend-icon="mdi-calendar" class="text-capitalize d-none d-md-flex">Reserva</v-btn>
+      <v-btn text to="/servicios" prepend-icon="mdi-format-list-bulleted-triangle" class="text-capitalize d-none d-md-flex">Servicios</v-btn>
+      <v-btn text to="/sobre-nosotros" prepend-icon="mdi-account-group" class="text-capitalize d-none d-md-flex">Sobre nosotros</v-btn>
+      <v-btn text to="/sucursales" prepend-icon="mdi-store" class="text-capitalize d-none d-md-flex">Sucursales</v-btn>
+      <v-btn text href="#tienda-online" prepend-icon="mdi-store" class="text-capitalize d-none d-md-flex">Tienda online</v-btn>
 
-  <v-menu class="d-none d-md-flex">
-  <template v-slot:activator="{ props }">
-    <v-btn class="text-capitalize" prepend-icon="mdi-school" text v-bind="props">
-      Cursos
-    </v-btn>
-  </template>
-  <v-list>
-    <v-list-item to="/cursos" v-for="(item, index) in courseData" :key="index" :value="index">
-      <v-list-item-title>{{ item.name }}</v-list-item-title>
-    </v-list-item>
-  </v-list>
-</v-menu>
+      <v-menu class="d-none d-md-flex">
+        <template v-slot:activator="{ props }">
+          <v-btn class="text-capitalize" prepend-icon="mdi-school" text v-bind="props">
+            Cursos
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/cursos" v-for="(item, index) in courseData" :key="index" :value="index">
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-  <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-  <!-- Iconos de Redes Sociales -->
-  <v-btn icon href="https://facebook.com" target="_blank" class="d-none d-md-flex">
-    <v-icon>mdi-facebook</v-icon>
-  </v-btn>
-  <v-btn icon href="https://twitter.com" target="_blank" class="d-none d-md-flex">
-    <v-icon>mdi-twitter</v-icon>
-  </v-btn>
-  <v-btn icon href="https://instagram.com" target="_blank" class="d-none d-md-flex">
-    <v-icon>mdi-instagram</v-icon>
-  </v-btn>
-</v-app-bar>
-
+      <!-- Iconos de Redes Sociales -->
+      <v-btn icon density="comfortable" class="ml-2" href="https://www.instagram.com/barberiahernandeztv/"
+             target="_blank">
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
+      <v-btn bg-color="#FFFFFF00" icon density="comfortable"
+             href="https://www.facebook.com/profile.php?id=100065259200120" target="_blank">
+        <v-icon>mdi-facebook</v-icon>
+      </v-btn>
+      <v-btn icon density="comfortable" class="ml-2" href="https://www.youtube.com/channel/UCRVTESNlCnAS45J4DEBi7tQ"
+             target="_blank">
+        <v-icon>mdi-youtube</v-icon>
+      </v-btn>
+      <v-btn icon density="comfortable" class="ml-2" href="https://www.tiktok.com/@barberiahernandeztv" target="_blank">
+        <v-icon>
+          <img src="@/assets/logo-tiktok-1.svg" alt="TikTok Icon" style="width: 20px; height: 20px;">
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app temporary>
-  <v-list dense>
-    <v-list-item to="/">
-      <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
-      <v-list-item-title>Inicio</v-list-item-title>
-    </v-list-item>
-    <v-list-item href="https://reservasbh.simplifies.cl">
-      <v-list-item-icon><v-icon>mdi-calendar</v-icon></v-list-item-icon>
-      <v-list-item-title>Reserva</v-list-item-title>
-    </v-list-item>
-    <v-list-item to="/servicios">
-      <v-list-item-icon><v-icon>mdi-format-list-bulleted-triangle</v-icon></v-list-item-icon>
-      <v-list-item-title>Servicios</v-list-item-title>
-    </v-list-item>
-    <v-list-item to="/sobre-nosotros">
-      <v-list-item-icon><v-icon>mdi-account-group</v-icon></v-list-item-icon>
-      <v-list-item-title>Sobre nosotros</v-list-item-title>
-    </v-list-item>
-    <v-list-item to="/sucursales">
-      <v-list-item-icon><v-icon>mdi-store</v-icon></v-list-item-icon>
-      <v-list-item-title>Sucursales</v-list-item-title>
-    </v-list-item>
-    <v-list-item href="#tienda-online">
-      <v-list-item-icon><v-icon>mdi-store</v-icon></v-list-item-icon>
-      <v-list-item-title>Tienda online</v-list-item-title>
-    </v-list-item>
+      <v-list dense>
+        <v-list-item to="/">
+          <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
+          <v-list-item-title>Inicio</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://reservasbh.simplifies.cl">
+          <v-list-item-icon><v-icon>mdi-calendar</v-icon></v-list-item-icon>
+          <v-list-item-title>Reserva</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/servicios">
+          <v-list-item-icon><v-icon>mdi-format-list-bulleted-triangle</v-icon></v-list-item-icon>
+          <v-list-item-title>Servicios</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/sobre-nosotros">
+          <v-list-item-icon><v-icon>mdi-account-group</v-icon></v-list-item-icon>
+          <v-list-item-title>Sobre nosotros</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/sucursales">
+          <v-list-item-icon><v-icon>mdi-store</v-icon></v-list-item-icon>
+          <v-list-item-title>Sucursales</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="#tienda-online">
+          <v-list-item-icon><v-icon>mdi-store</v-icon></v-list-item-icon>
+          <v-list-item-title>Tienda online</v-list-item-title>
+        </v-list-item>
 
-    <!-- Cursos dentro del Drawer -->
-    <v-subheader>Cursos</v-subheader>
-    <v-list-item to="/cursos" v-for="(item, index) in courseData" :key="index" :value="index">
-      <v-list-item-title>{{ item.name }}</v-list-item-title>
-    </v-list-item>
-  </v-list>
+        <!-- Cursos dentro del Drawer -->
+        <v-subheader>Cursos</v-subheader>
+        <v-list-item to="/cursos" v-for="(item, index) in courseData" :key="index" :value="index">
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
 
       <!-- Iconos de Redes Sociales en el Drawer -->
       <v-divider></v-divider>
       <v-list-item>
-        <v-btn icon href="https://facebook.com" target="_blank">
-          <v-icon>mdi-facebook</v-icon>
-        </v-btn>
-        <v-btn icon href="https://twitter.com" target="_blank">
-          <v-icon>mdi-twitter</v-icon>
-        </v-btn>
-        <v-btn icon href="https://instagram.com" target="_blank">
-          <v-icon>mdi-instagram</v-icon>
+        <!-- Iconos de Redes Sociales -->
+        <v-btn icon density="comfortable" class="ml-2" href="https://www.instagram.com/barberiahernandeztv/"
+            target="_blank">
+            <v-icon>mdi-instagram</v-icon>
+          </v-btn>
+          <v-btn bg-color="#FFFFFF00" icon density="comfortable" class="ml-2"
+            href="https://www.facebook.com/profile.php?id=100065259200120" target="_blank">
+            <v-icon>mdi-facebook</v-icon>
+          </v-btn>
+          <v-btn icon density="comfortable" class="ml-2" href="https://www.youtube.com/channel/UCRVTESNlCnAS45J4DEBi7tQ"
+            target="_blank">
+            <v-icon>mdi-youtube</v-icon>
+          </v-btn>
+          <v-btn icon density="comfortable" class="ml-2" href="https://www.tiktok.com/@barberiahernandeztv" target="_blank">
+          <v-icon>
+            <img src="@/assets/logo-tiktok.svg" alt="TikTok Icon" style="width: 20px; height: 20px;">
+          </v-icon>
         </v-btn>
       </v-list-item>
     </v-navigation-drawer>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,65 +1,66 @@
 <template>
-    <v-footer color="grey-darken-4"  class="white--text mb-o pb-0">
-      <v-container fluid>
-        <!-- Contenido Principal del Footer -->
-        <v-row class="mb-4">
-          <v-col cols="12" md="1"></v-col>
-          <v-col cols="12" md="4">
-            <!-- Contenido centrado para pantallas pequeñas y alineado a la izquierda en pantallas más grandes -->
-        <v-list bg-color="grey-darken-4" align="left">
-          <v-list-subheader class="text-white text-subtitle-1" inset>Nuestras Sucursales</v-list-subheader>
-        <v-list-item
-        v-for="card in cards"
-        :key="card.title"
-        :subtitle="card.location"
-        :title="card.title"
-      >
-        <template v-slot:prepend>
-          <v-avatar :color="card.color">
-            <v-icon color="orange">{{ card.icon }}</v-icon>
-          </v-avatar>
-        </template>
+  <v-footer color="grey-darken-4" class="white--text mb-o pb-0">
+    <v-container fluid>
+      <!-- Contenido Principal del Footer -->
+      <v-row class="mb-4">
+        <v-col cols="12" md="1"></v-col>
+        <v-col cols="12" md="4">
+          <!-- Contenido centrado para pantallas pequeñas y alineado a la izquierda en pantallas más grandes -->
+          <v-list bg-color="grey-darken-4" align="left">
+            <v-list-subheader class="text-white text-subtitle-1" inset>Nuestras Sucursales</v-list-subheader>
+            <v-list-item v-for="card in cards" :key="card.title" :subtitle="card.location" :title="card.title">
+              <template v-slot:prepend>
+                <v-avatar :color="card.color">
+                  <v-icon color="orange">{{ card.icon }}</v-icon>
+                </v-avatar>
+              </template>
 
-      </v-list-item>
-    </v-list>
-          </v-col>
-          <v-col cols="12" md="3"></v-col>
-          <v-col cols="12" md="3">
-     
-            <v-list-subheader class="text-white text-subtitle-1" inset>Síguenos en Nuestras Redes</v-list-subheader>
-          <!-- Iconos de Redes Sociales -->
-        <v-btn bg-color="#FFFFFF00" icon  density="comfortable" href="https://facebook.com" target="_blank">
-        <v-icon>mdi-facebook</v-icon>
-      </v-btn>
-      <v-btn icon density="comfortable" class="ml-2" href="https://twitter.com" target="_blank">
-        <v-icon>mdi-twitter</v-icon>
-      </v-btn>
-      <v-btn icon density="comfortable" class="ml-2" href="https://instagram.com" target="_blank">
-        <v-icon>mdi-instagram</v-icon>
-      </v-btn>
-   
-          </v-col>
-        </v-row>
-  
-        <!-- Copyright, centrado y debajo de todo -->
-        <v-row class="mb-0 pb-0">
-          <v-col cols="12" class="text-center">
-            <v-divider></v-divider>
-            <div class="pt-4">
+            </v-list-item>
+          </v-list>
+        </v-col>
+        <v-col cols="12" md="3"></v-col>
+        <v-col cols="12" md="3">
+
+          <v-list-subheader class="text-white text-subtitle-1" inset>Síguenos en Nuestras Redes</v-list-subheader>
+          <v-btn icon density="comfortable" class="ml-2" href="https://www.instagram.com/barberiahernandeztv/"
+            target="_blank">
+            <v-icon>mdi-instagram</v-icon>
+          </v-btn>
+          <v-btn bg-color="#FFFFFF00" icon density="comfortable" class="ml-2"
+            href="https://www.facebook.com/profile.php?id=100065259200120" target="_blank">
+            <v-icon>mdi-facebook</v-icon>
+          </v-btn>
+          <v-btn icon density="comfortable" class="ml-2" href="https://www.youtube.com/channel/UCRVTESNlCnAS45J4DEBi7tQ"
+            target="_blank">
+            <v-icon>mdi-youtube</v-icon>
+          </v-btn>
+          <v-btn icon density="comfortable" class="ml-2" href="https://www.tiktok.com/@barberiahernandeztv" target="_blank">
+          <v-icon>
+            <img src="@/assets/logo-tiktok.svg" alt="TikTok Icon" style="width: 24px; height: 24px;">
+          </v-icon>
+        </v-btn>
+        </v-col>
+      </v-row>
+
+      <!-- Copyright, centrado y debajo de todo -->
+      <v-row class="mb-0 pb-0">
+        <v-col cols="12" class="text-center">
+          <v-divider></v-divider>
+          <div class="pt-4">
             &copy; {{ new Date().getFullYear() }} Todos los derecho reservados. Barberías Hernandez</div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-footer>
-  </template>
-  
-  <script>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
 import axios from "axios";
 
-  export default {
-    name: 'AppFooter',
+export default {
+  name: 'AppFooter',
 
-    data() {
+  data() {
     return {
       curriculum: null,
       hover: false,
@@ -108,26 +109,25 @@ import axios from "axios";
           image: 'academia.jpg', // Asume que esta es la imagen en la carpeta assets
           
         },*/
-        
-      ],     
+
+      ],
     };
   },
   mounted() {
     axios
-        .get('https://api2.simplifies.cl/api/business-branch-academy')
-        .then((response) => {          
-          //console.log(this.response)
-          this.cards = response.data.business;
-          console.log(this.cards);
-          //console.log(this.student)
-        });
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .v-footer {
-    padding: 1rem 0;
-  }
-  </style>
-  
+      .get('https://api2.simplifies.cl/api/business-branch-academy')
+      .then((response) => {
+        //console.log(this.response)
+        this.cards = response.data.business;
+        console.log(this.cards);
+        //console.log(this.student)
+      });
+  },
+};
+</script>
+
+<style scoped>
+.v-footer {
+  padding: 1rem 0;
+}
+</style>
